3913b5a997be6e3b56cc9b4241f0d573561ff21c
srajsonu <43319626+srajsonu@users.noreply.github.com>
2019-03-22 00:04:07 +0530

    Create custom UI Button

---
+++ Custom UI Button/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Custom UI Button
+//
+//  Created by ARY@N on 21/03/19.
+//  Copyright © 2019 ARYAN. All rights reserved.
+//
+
+import UIKit
+
+enum Colors {
+    static let coolBlue = UIColor(red: 42/255, green: 126/255, blue: 254/255, alpha: 1.0)
+}
--- Custom UI Button/ViewController.swift
+++ Custom UI Button/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var bottomCustomButton = CustomButton()
+    var topCustomButton = CustomButton()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupBottomButtonConstraints()
+        addActionToBottomButton()
+        bottomCustomButton.setTitle("Bottom", for: .normal)
     }
-
-
+    
+    
+    func setupBottomButtonConstraints() {
+        view.addSubview(bottomCustomButton)
+        bottomCustomButton.translatesAutoresizingMaskIntoConstraints = false
+        bottomCustomButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        bottomCustomButton.widthAnchor.constraint(equalToConstant: 280).isActive = true
+        bottomCustomButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        bottomCustomButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -200).isActive = true
+    }
+    
+    
+    func addActionToBottomButton() {
+        bottomCustomButton.addTarget(self, action: #selector(bottomButtonTapped), for: .touchUpInside)
+    }
+    
+    
+ 
+    @IBAction func topButtonTapped(_ sender: Any) {
+        topCustomButton.shake()
+    }
+    
+    
+    @objc func bottomButtonTapped() {
+        bottomCustomButton.shake()
+    }
 }
 
